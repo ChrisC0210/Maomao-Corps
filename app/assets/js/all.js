51096eb9cc638c8cac0b30081bed14b9131bb5a5
chris chen <christinachen149@gmail.com>
2022-09-05 02:12:11 +0800

    Add html, css, img

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,8 @@
 $(function() {
   console.log('Hello Bootstrap5');
+  $(".login-click").click(function () {
+    $(this).text(function(i, text){
+        return text === "登入/註冊" ? "登出" : "登入/註冊";
+    })
+ });
 });
